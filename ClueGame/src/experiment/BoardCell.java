2d829e0b00629c7d122b00f7ba66aa15dbca77ad
Mona W <dwade@mines.edu>
2017-10-06 15:28:21 -0600

    Added some changes to the code.

--- ClueGame/src/experiment/BoardCell.java
+++ ClueGame/src/experiment/BoardCell.java
@@ -2,5 +2,13 @@ package experiment;
 
 public class BoardCell {
 	// two member variables to represent the row and column
-	int row, col; 
+	public int row, col;
+	
+	// constructor (optional)
+	public BoardCell() {
+		row = 0;
+		col = 0;
+	} 
+	
+	
 }
