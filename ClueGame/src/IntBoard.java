8ce30d5f605120ba3c141482b572cb30c9829672
Mona W <dwade@mines.edu>
2017-10-06 09:58:27 -0600

    Made changes to the IntBoard.java file. I have added in the data structure containing board cells.

--- ClueGame/src/IntBoard.java
+++ ClueGame/src/IntBoard.java
@@ -1,20 +1,24 @@
+import java.util.*;
+import experiment.BoardCell;
 
 public class IntBoard {
 	
 	int startCell, pathLength; 
+	// data  structure containing the board cells
+	private Map<BoardCell, Set<BoardCell>> adjMtx;
 	
-		public IntBoard(int startCell, int pathLength) {
+	public IntBoard(int startCell, int pathLength) {
 		super();
 		this.startCell = startCell;
 		this.pathLength = pathLength;
 	}
-	
+
 	public void calcAdjiancies() {
-		
+
 	}
-	
-	public void calcTargets(startCell, pathLength) { 
-		
+
+	public void calcTargets(int startCell, int pathLength) { 
+
 	}
-	
+
 }
--- ClueGame/src/experiment/BoardCell.java
+++ ClueGame/src/experiment/BoardCell.java
@@ -1,9 +1,6 @@
 package experiment;
 
 public class BoardCell {
-	
+	// two member variables to represent the row and column
 	int row, col; 
-	
-	
-	
 }
