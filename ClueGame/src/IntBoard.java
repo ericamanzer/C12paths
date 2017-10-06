9ef5e6895ef307037922f50ff946c465adc5feb8
Mona W <dwade@mines.edu>
2017-10-06 10:47:22 -0600

    Added more objects required for the IntBoard. Also initialized them in the constructor.

--- ClueGame/src/IntBoard.java
+++ ClueGame/src/IntBoard.java
@@ -1,19 +1,36 @@
 import java.util.*;
 import experiment.BoardCell;
 
+// For this exercise, you will write and test the adjacency and 
+// target calculation algorithms for a simple 4x4 grid.
+
 public class IntBoard {
 	
 	int startCell, pathLength; 
 	// data  structure containing the board cells
 	private Map<BoardCell, Set<BoardCell>> adjMtx;
+	// visited list
+	private Set<BoardCell> visited;
+	// targets list
+	private Set<BoardCell> targets;
+	// grid - 2D array 
+	private BoardCell[][] grid;
 	
-	public IntBoard(int startCell, int pathLength) {
+	// default constructor
+	public IntBoard() 
+	{
 		super();
-		this.startCell = startCell;
-		this.pathLength = pathLength;
+		startCell = 0;
+		pathLength = 0;
+		// A good application of an adjacency list is a map
+		// initializing a hashmap using a static initializer
+		adjMtx = new HashMap<BoardCell, Set<BoardCell>>();
+		visited = new HashSet<BoardCell>();
+		targets = new HashSet<BoardCell>();
+		grid = new BoardCell [4][4];
 	}
 
-	public void calcAdjiancies() {
+	public void calcAdjancies() {
 
 	}
 
