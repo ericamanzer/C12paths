799934ce7a78e857544d64e8d28654a8afb1bae2
Mona W <dwade@mines.edu>
2017-10-22 13:14:03 -0600

    Adding the Files need for BoardAdjTargetsTest

---
+++ ClueGame/src/test/BoardAdjTargetsTests.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author dwade
+ *
+ */
+public class BoardAdjTargetsTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
