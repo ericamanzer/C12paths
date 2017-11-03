d8ceb43163c348f961b0d161c70260f13370fc5a
Mona W <dwade@mines.edu>
2017-11-03 14:50:15 -0600

    Adding the PlayerTest2 that has the completingDeckTest

--- ClueGame/src/clueGame/Card.java
+++ ClueGame/src/clueGame/Card.java
@@ -2,19 +2,28 @@ package clueGame;
 
 public class Card {
 
-	private String carName; 
+	private String cardName; 
 	private CardType cardType; 
 	
 	public Card(String carName, CardType cardType) {
 		super();
-		this.carName = carName;
+		this.cardName = carName;
 		this.cardType = cardType;
 	}
 
-	
-
+	/*
 	public boolean equals() {
 		return true; // FIXME !!!
 	}
+	*/
 	
+	public CardType getCardType()
+	{
+		return this.cardType;
+	}
+	
+	public String getCardname()
+	{
+		return this.cardName;
+	}
 }
--- ClueGame/src/test/PlayerTest2.java
+++ ClueGame/src/test/PlayerTest2.java
@@ -5,28 +5,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import java.util.*;
 public class PlayerTest2 {
-
+	private static Board board;
 	@Test
 	public void completingDeckTest() {
 		Set<Card> deck = new HashSet<Card>();
-		deck = getDeck();
+		deck = board.getDeck();
 		
 		// Testing the size of the deck
 		assertEquals(21, deck.size());
-		int person, weapon, room = 0;
+		int person = 0;
+		int room = 0;
+		int weapon = 0;
+		int exist = 0;
 		for ( Card card : deck )
 		{
 			if ( card.getCardType() == CardType.PERSON)
 			{
-				person++;
+				person ++;
 			}
 			if ( card.getCardType() == CardType.ROOM)
 			{
-				room++;
+				room ++;
 			}
 			if ( card.getCardType() == CardType.WEAPON)
 			{
-				weapon++;
+				weapon ++;
 			}
 		}
 		// Testing that the correct number of cards for each kind exist
@@ -35,6 +38,11 @@ public class PlayerTest2 {
 		assertEquals(9, room);
 		
 		// Testing to see if a particular card exist
+		if ( person >= 1 && room >= 1 && weapon >= 1)
+		{
+			exist ++;
+		}
+		assertEquals(1, exist);
 		
 	}
 
