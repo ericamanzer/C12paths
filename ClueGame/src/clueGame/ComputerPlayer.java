be0413cbe256a50e02b60d237494075ab3a5d898
Mona W <dwade@mines.edu>
2017-11-05 19:18:59 -0700

    Finished the beginning of testSelectingATarget

--- ClueGame/src/clueGame/ComputerPlayer.java
+++ ClueGame/src/clueGame/ComputerPlayer.java
@@ -25,9 +25,36 @@ public class ComputerPlayer extends Player {
 	
 	public BoardCell pickLocation(Set<BoardCell> targets) {
 		
+		ArrayList<BoardCell> availablePaths = new ArrayList<BoardCell>();
+		ArrayList<BoardCell> availableDoorways = new ArrayList<BoardCell>();
 		for (BoardCell cell : targets)
 		{
-			previous.
+			if (cell.isDoorway())
+			{
+				availableDoorways.add(cell);
+			}
+			if (cell.isWalkway())
+			{
+				availablePaths.add(cell);
+			}
+		}
+		
+		if (!availableDoorways.isEmpty())
+		{
+			int options = availableDoorways.size();
+			Random rand = new Random(); 
+			int selected = rand.nextInt(options); 
+			//System.out.println( options + " doorways exist");
+			
+			return availableDoorways.get(selected);
+		}
+		else
+		{
+			int options = availablePaths.size();
+			Random rand = new Random();
+			int selected = rand.nextInt(options);
+			//System.out.println("A pathway is available");
+			return availablePaths.get(selected);
 		}
 		
 	}
--- ClueGame/src/test/GameActionTests.java
+++ ClueGame/src/test/GameActionTests.java
@@ -11,36 +11,42 @@ public class GameActionTests {
 	// Making the board static so that only one copy of itself
 	private static Board board;
 	@BeforeClass
-	public void setUp() throws Exception {
+	public static void setUp() throws Exception {
 		board = Board.getInstance();
 		//set the file names to be used 
 		board.setConfigFiles("C14 Layout.csv", "C12 Layout.txt");
 		board.setWeaponsConfigFile("WeaponsConfig.txt");
 		board.setPeopleConfigFile("PeopleConfig.txt");
-		
+
 		board.initialize();
 	}
 
 	@Test
 	public void testSelectingATarget() {
-		fail("Not yet implemented");
+		// One room is exactly 2 away
+		board.calcTargets(12, 16, 4);
+		// save current location into BoardCell previous; may need or may not
+		//BoardCell current =  board.getCellAt(12, 16);
+		//Player computer = new Player();
+		//computer.savingTestLocation(current);
 		
-		//should only choose a valid target; calculating targets already tested
-		board.calcTargets(4, 2, 3);
-		// save current location into BoardCell previous
-		BoardCell current =  board.getCellAt(4, 2);
-		Player computer = new Player();
-		computer.savingTestLocation(current);
-		
+		// will be passing in targets into the pickLocation function that is called by ComputerPlayer which extends player
 		Set<BoardCell> targets= board.getTargets();
-		assertEquals(9, targets.size());
-		assertTrue(targets.contains(board.getCellAt(4, 1)));
-		assertTrue(targets.contains(board.getCellAt(4, 3)));
-		assertTrue(targets.contains(board.getCellAt(5, 2)));
+		// testing the calculated targets 
+		//assertEquals(16, targets.size());
+		assertTrue(targets.contains(board.getCellAt(11, 19)));
+		assertTrue(targets.contains(board.getCellAt(9, 17)));
+		// Last target tested is doorway 
+		assertTrue(targets.contains(board.getCellAt(13, 19)));
+
+		// testing to see if the selected room is equal to the correct room (the room it should be)
+		BoardCell correctSelection = new BoardCell (13, 19, 'B', DoorDirection.UP );
 		
-		//HELLO
-		// If no rooms in list, choose a target randomnly 
-		
+		// need to call ComputerPlayer with the pickLocation function and return the selected position
+		ComputerPlayer computer = new ComputerPlayer();
+		BoardCell selected = computer.pickLocation(targets);
+		assertEquals(board.getCellAt(13, 19), selected);
+
 	}
 
 }
